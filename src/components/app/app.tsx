9f3d3364269ca05b9887dcbc744050bc45e43d35
Ksu Sal <ksuyvas@yandex.ru>
2026-07-20 06:30:59 +0300

    роутинг и запросы к серверу

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,31 +1,100 @@
-import { ConstructorPage } from '@pages';
-import '../../index.css';
+import {
+  ConstructorPage,
+  Feed,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders,
+  NotFound404
+} from '@pages';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { AppHeader, Modal, OrderInfo, IngredientDetails } from '@components';
+import { Preloader } from '@ui';
+import { useDispatch, useSelector } from '../../services/store';
+import { fetchIngredients } from '../../services/slices/ingredientsSlice';
+import { useEffect } from 'react';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
-import { Preloader } from '@ui';
-
 const App = () => {
-  /** TODO: взять переменные из стора */
-  const isIngredientsLoading = false;
-  const ingredients = [];
-  const error = null;
+  const location = useLocation();
+  const background = location.state?.background;
+  const dispatch = useDispatch();
+
+  const {
+    data: ingredients,
+    isLoading,
+    error
+  } = useSelector((state) => state.ingredients);
+
+  useEffect(() => {
+    dispatch(fetchIngredients());
+  }, [dispatch]);
 
   return (
     <div className={styles.app}>
       <AppHeader />
-      {isIngredientsLoading ? (
-        <Preloader />
-      ) : error ? (
-        <div className={`${styles.error} text text_type_main-medium pt-4`}>
-          {error}
-        </div>
-      ) : ingredients.length > 0 ? (
-        <ConstructorPage />
-      ) : (
-        <div className={`${styles.title} text text_type_main-medium pt-4`}>
-          Нет игредиентов
-        </div>
+      <Routes location={background || location}>
+        <Route
+          path='/'
+          element={
+            isLoading ? (
+              <Preloader />
+            ) : error ? (
+              <div
+                className={`${styles.error} text text_type_main-medium pt-4`}
+              >
+                {error}
+              </div>
+            ) : ingredients.length > 0 ? (
+              <ConstructorPage />
+            ) : (
+              <div
+                className={`${styles.title} text text_type_main-medium pt-4`}
+              >
+                Нет игредиентов
+              </div>
+            )
+          }
+        />
+        <Route path='/feed' element={<Feed />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/profile/orders' element={<ProfileOrders />} />
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+
+      {background && (
+        <Routes>
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal title='Детали заказа' onClose={() => {}}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal title='Детали ингредиента' onClose={() => {}}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <Modal title='Детали заказа' onClose={() => {}}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+        </Routes>
       )}
     </div>
   );
--- src/pages/reset-password/reset-password.tsx
+++ src/pages/reset-password/reset-password.tsx
@@ -21,11 +21,11 @@ export const ResetPassword: FC = () => {
       .catch((err) => setError(err));
   };
 
-  useEffect(() => {
-    if (!localStorage.getItem('resetPassword')) {
-      navigate('/forgot-password', { replace: true });
-    }
-  }, [navigate]);
+  //  useEffect(() => {
+  //   if (!localStorage.getItem('resetPassword')) {
+  //      navigate('/forgot-password', { replace: true });
+  //    }
+  //  }, [navigate]);
 
   return (
     <ResetPasswordUI
